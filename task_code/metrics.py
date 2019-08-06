95e42c2809c53824734b436843f210e8a611d380
anatshk <anat.shk@gmail.com>
2019-08-06 19:08:54 +0300

    requiremants file

---
+++ task_code/metrics.py
@@ -0,0 +1,12 @@
+"""
+Metrics
+"""
+
+
+def pix_to_pix_rmse(img1, img2):
+    """
+    Calculates 
+    :param img1:
+    :param img2:
+    :return:
+    """
---
+++ task_code/utils.py
@@ -0,0 +1,7 @@
+"""
+Utils file
+"""
+
+import tarfile
+
+
