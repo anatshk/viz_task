5166e9aec9d3c7595011e681027c8924221782fa
anatshk <anat.shk@gmail.com>
2019-08-06 22:36:45 +0300

    baseline calculation write to csv

--- task_code/calculate_baseline.py
+++ task_code/calculate_baseline.py
@@ -6,6 +6,7 @@ Runs on given dataset, calculates RMSE for jpg and bmp images.
 
 import numpy as np
 import os
+import pandas as pd
 
 from task_code.metrics import pix_to_pix_rmse
 from task_code.utils import get_image
@@ -21,7 +22,7 @@ all_files = os.listdir(PATH_TO_DATA)
 unique_img_num = len(all_files) // 2
 
 # go over files, load bmp and jpg, calculate rmse
-all_img_rmse = np.empty((unique_img_num,))
+all_img_rmse = pd.DataFrame(columns=['image_name', 'pixelwise_rmse'])
 for file_ix in range(0, len(all_files), 2):
     img_ix = file_ix // 2
     fname = all_files[file_ix][:-4]
@@ -32,8 +33,12 @@ for file_ix in range(0, len(all_files), 2):
     jpg_img = get_image(jpg)
     bmp_img = get_image(bmp)
     img_rmse = pix_to_pix_rmse(bmp_img, jpg_img)
-    all_img_rmse[img_ix] = img_rmse
-    print('=== {}/{} - {}, RMSE - {} ==='.format(img_ix, unique_img_num, fname, img_rmse))
+    all_img_rmse = all_img_rmse.append(pd.Series(data={'image_name': fname, 'pixelwise_rmse': img_rmse}), ignore_index=True)
+    print('=== {}/{} - {}, RMSE - {:.3f} ==='.format(img_ix, unique_img_num, fname, img_rmse))
 
-print('=== FINISHED, MEAN RMSE is {:.2f} ==='.format(all_img_rmse.mean()))
+mean_rmse = all_img_rmse['pixelwise_rmse'].mean()
+print('=== FINISHED, MEAN RMSE is {:.3f} ==='.format(mean_rmse))
+all_img_rmse = all_img_rmse.append(pd.Series(data={'image_name': 'total', 'pixelwise_rmse': mean_rmse}), ignore_index=True)
+
+all_img_rmse.to_csv('baseline_rmse.csv', index=False)
 
